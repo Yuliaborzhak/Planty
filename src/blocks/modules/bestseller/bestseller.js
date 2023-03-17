0e5da417a3346e47394f920435ced724cc1c0ff6
Yulia Borzhak <yuliaborzhak@gmail.com>
2023-03-17 12:48:12 +0100

    hero main

--- src/blocks/modules/bestseller/bestseller.js
+++ src/blocks/modules/bestseller/bestseller.js
@@ -1,20 +1,32 @@
 import Swiper from 'swiper';
+//
+document.addEventListener("DOMContentLoaded", () => {
 
-const swiper = new Swiper('.swiper', {
-    spaceBetween: 16,
-    effect: "fade",
-    breakpoints: {
-        575: {
-            // slidesPerView: 2,
-            spaceBetween: 20,
+    console.log(1);
+
+    const swiper = new Swiper('.swiper', {
+        spaceBetween: 10,
+        // effect: "fade",
+        // centeredSlides: true,
+        slidesPerView: "auto",
+        breakpoints: {
+            // 575: {
+            //     // slidesPerView: 2,
+            //     spaceBetween: 20,
+            // },
+            768: {
+                // slidesPerView: 4,
+                spaceBetween: 22,
+                // slidesPerView: 34,
+                // centeredSlides: false,
+            },
+            992: {
+                // slidesPerView: 5,
+                spaceBetween: 32,
+                // slidesPerView: 3.5,
+                // centeredSlides: false,
+            },
         },
-        768: {
-            // slidesPerView: 4,
-            spaceBetween: 30,
-        },
-        992: {
-            // slidesPerView: 5,
-            spaceBetween: 44,
-        },
-    },
+    });
 });
+
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/newsletter/newsletter";
+import "%modules%/bestseller/bestseller";
 import "%modules%/footer/footer";
